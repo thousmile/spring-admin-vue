81d87aa982a9a69a21908f7050a402566f576615
隔壁小王 <932560435@qq.com>
2020-01-15 15:27:45 +0800

    Update SpringMvcConfiguration.java

--- spring-restful-api/src/main/java/com/ifsaid/shark/common/config/SpringMvcConfiguration.java
+++ spring-restful-api/src/main/java/com/ifsaid/shark/common/config/SpringMvcConfiguration.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,14 +19,13 @@ import org.springframework.format.Formatter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * All rights Reserved, Designed By www.ifsaid.com
@@ -44,9 +45,10 @@ import java.util.Locale;
 @AutoConfigureBefore(WebSecurityConfiguration.class)
 public class SpringMvcConfiguration implements WebMvcConfigurer {
 
-
     private static final String DEFAULT_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
     private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+
     private static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
 
     /**
@@ -68,12 +70,16 @@ public class SpringMvcConfiguration implements WebMvcConfigurer {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         JavaTimeModule javaTimeModule = new JavaTimeModule();
+
         javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
-        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
         javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
+
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
         javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
+
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
         javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
+
         objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
@@ -86,21 +92,19 @@ public class SpringMvcConfiguration implements WebMvcConfigurer {
         return new Formatter<Date>() {
 
             @Override
-            public Date parse(String text, Locale locale) throws ParseException {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            public Date parse(String text, Locale locale) {
                 Date date = null;
                 try {
-                    date = sdf.parse(text);
+                    date = DateUtils.parseDate(text, locale, DEFAULT_DATE_TIME_PATTERN);
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    log.error(e.getMessage());
                 }
                 return date;
             }
 
             @Override
-            public String print(Date object, Locale locale) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                return sdf.format(object);
+            public String print(Date date, Locale locale) {
+                return DateFormatUtils.format(date, DEFAULT_DATE_TIME_PATTERN, TimeZone.getDefault(), locale);
             }
         };
     }
@@ -109,13 +113,28 @@ public class SpringMvcConfiguration implements WebMvcConfigurer {
     public Formatter<LocalDate> localDateFormatter() {
         return new Formatter<LocalDate>() {
             @Override
-            public LocalDate parse(String text, Locale locale) throws ParseException {
-                return LocalDate.parse(text, DateTimeFormatter.ISO_LOCAL_DATE);
+            public LocalDate parse(String text, Locale locale) {
+                return LocalDate.parse(text, DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN, locale));
             }
 
             @Override
             public String print(LocalDate object, Locale locale) {
-                return DateTimeFormatter.ISO_LOCAL_DATE.format(object);
+                return DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN, locale).format(object);
+            }
+        };
+    }
+
+    @Bean
+    public Formatter<LocalTime> localTimeFormatter() {
+        return new Formatter<LocalTime>() {
+            @Override
+            public LocalTime parse(String text, Locale locale) {
+                return LocalTime.parse(text, DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN, locale));
+            }
+
+            @Override
+            public String print(LocalTime object, Locale locale) {
+                return DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN, locale).format(object);
             }
         };
     }
@@ -126,13 +145,12 @@ public class SpringMvcConfiguration implements WebMvcConfigurer {
 
             @Override
             public String print(LocalDateTime localDateTime, Locale locale) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                return formatter.format(localDateTime);
+                return DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN, locale).format(localDateTime);
             }
 
             @Override
-            public LocalDateTime parse(String text, Locale locale) throws ParseException {
-                return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            public LocalDateTime parse(String text, Locale locale) {
+                return LocalDateTime.parse(text, DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN, locale));
             }
         };
     }
